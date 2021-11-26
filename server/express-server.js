74a37d5c41936ffa95f0ef4c5c499630b37bc5b9
Tuomas Välimäki <t.valimaki@gmail.com>
2021-11-26 20:31:28 +0200

    Fix starting script

--- server/express-server.js
+++ server/express-server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'build');
 console.log(publicPath)
 app.use(express.static(publicPath));
 app.listen(port, () => {
