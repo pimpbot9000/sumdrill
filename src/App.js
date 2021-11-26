19d8dc362d13bdb65db1d820f5b10f310edcf957
Tuomas Välimäki <t.valimaki@gmail.com>
2021-11-26 20:27:15 +0200

    first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { useEffect, useState } from 'react'
+
+function getRndInteger(min, max) {
+  let result = 0
+  do {
+    result = Math.floor(Math.random() * (max - min + 1)) + min;    
+  } while (result === 0)
+
+  return result
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [numbers, setNumbers] = useState([1, 1])
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    createNewNumbers()
+  }, [])
+
+
+
+  const createNewNumbers = () => {
+    setNumbers([getRndInteger(-5, 10), getRndInteger(-5, 5)])
+  }
+
+  const onClickHandler = (res) => {
+    if (res === numbers.reduce((acc, elem) => acc + elem, 0)) {
+      setResults([...results, 1])
+    } else {
+      setResults([...results, -1])
+    }
+    createNewNumbers()
+  }
+
+  return (<>
+
+    <div className="Container">
+      <Container>
+        <Row>
+          <Col>
+            <div className="Blackboard">
+              <Expression numbers={numbers} />
+            </div>
+          </Col>
+        </Row>
+        <Candidates numbers={numbers} onClick={onClickHandler} />
+
+        <Scores results={results} />
+      </Container>
+
     </div>
+  </>
+
   );
 }
 
+const Scores = ({ results }) => {
+  const scores = results.map(r => { if (r === 1) { return (<>😃</>) } else { return (<>😢</>) } })
+
+  return <div className="Emoji">{scores}</div>
+}
+
+const generateCandidates = (result) => {
+
+  const candidates = new Set()
+  candidates.add(result)
+  do {
+    candidates.add(getRndInteger(-5, 5))
+  } while (candidates.size < 4)
+
+  const candidatesArray = [...candidates]
+  shuffleArray(candidatesArray)
+  return candidatesArray
+}
+
+const Candidates = ({ numbers, onClick }) => {
+  const candidates = generateCandidates(numbers[0] + numbers[1])
+  const col = candidates.map(c => <Col key="c"><div onClick={() => onClick(c)} className="Number"> {createNiceNumber(c)} </div></Col>)
+  return <Row>{col}</Row>
+
+}
+
+const Expression = ({ numbers }) => {
+
+  const niceNumbers = numbers.map(n => createNiceNumber(n))
+  
+  if (numbers[1] < 0) {
+
+    return (<div>
+      {niceNumbers[0]} + ({niceNumbers[1]})
+    </div>)
+  }
+
+  else {
+    return (<div>
+      {niceNumbers[0]} + {niceNumbers[1]}
+    </div>)
+  }
+}
+
+
+
+const createNiceNumber = (number) => {
+  if (number < 0) {
+    return "−" + Math.abs(number)
+  }
+  return number
+}
+
+/**
+ * 
+ * Copy pasted from Stack overflow. Shuffles an array in place. 
+ */
+const shuffleArray = (array) => {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+
 export default App;
+
