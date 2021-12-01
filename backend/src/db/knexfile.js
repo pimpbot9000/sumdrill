756a43908dc47953b06f57bd23744cbc3155747f
pimpbot9000 <t.valimaki@gmail.com>
2021-12-02 00:44:00 +0200

    Add ssl require

--- backend/src/db/knexfile.js
+++ backend/src/db/knexfile.js
@@ -46,7 +46,8 @@ module.exports = {
       host: 'ec2-54-84-142-90.compute-1.amazonaws.com',  //postgres
       database: 'd9ov1142fpfjnj',
       user:     'rubhuptnudsfeb',
-      password: '4c816140d0d2b36c38b409c7be6a384ae49c01c4682d1db44e15f58b534747ef'
+      password: '4c816140d0d2b36c38b409c7be6a384ae49c01c4682d1db44e15f58b534747ef',
+      ssl:true
     },
     pool: {
       min: 2,
