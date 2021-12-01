fce7bf7b4d76945ebeab80c975cc27c8b621f376
pimpbot9000 <t.valimaki@gmail.com>
2021-12-01 23:14:17 +0200

    random changes

--- src/services/scores.js
+++ src/services/scores.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.REACT_APP_API_URL
+const baseUrl = v1/api//process.env.REACT_APP_API_URL
 const axios = require('axios')
 //const camelcaseKeys = require('camelcase-keys')
 //const snakecaseKeys = require('snakecase-keys')
