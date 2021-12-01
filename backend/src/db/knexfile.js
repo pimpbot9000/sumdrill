95112d16f0b4d3235b9114cfb5dfe5f3a271ddbd
pimpbot9000 <t.valimaki@gmail.com>
2021-12-01 22:57:12 +0200

    change database settings

--- backend/src/db/knexfile.js
+++ backend/src/db/knexfile.js
@@ -2,10 +2,29 @@
 module.exports = {  
 
   development: {
-    client: 'postgresql',
+    client: 'pg', //or postgresql
     connection: {
       port: 5432,
-      host: 'postgres',
+      host: '127.0.0.1',  //postgres
+      database: 'knex',
+      user:     'knex',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: 'src/db/migrations'
+    }
+  },
+
+  docker_compose: {
+    client: 'pg',
+    connection: {
+      port: 5432,
+      host: 'postgres', 
       database: 'knex',
       user:     'knex',
       password: 'password'
@@ -23,9 +42,11 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      port: 5432,
+      host: 'ec2-54-84-142-90.compute-1.amazonaws.com',  //postgres
+      database: 'd9ov1142fpfjnj',
+      user:     'rubhuptnudsfeb',
+      password: '4c816140d0d2b36c38b409c7be6a384ae49c01c4682d1db44e15f58b534747ef'
     },
     pool: {
       min: 2,
