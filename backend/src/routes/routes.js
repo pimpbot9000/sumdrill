8283dea269b4c5fcaca075ece33710f27016117d
pimpbot9000 <t.valimaki@gmail.com>
2021-12-03 18:56:00 +0200

    Fix del route and cors

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-
+const cors = require('cors')
 const router = express.Router()
 const { createScore, getScores, deleteScores } = require('../controllers/score')
 
