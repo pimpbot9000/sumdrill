7967f2e25013451deba9ca0f044aa30fd9d2f7b1
pimpbot9000 <t.valimaki@gmail.com>
2021-12-03 18:06:00 +0200

    fix delete scores

--- backend/src/controllers/score.js
+++ backend/src/controllers/score.js
@@ -6,13 +6,13 @@ const createScore = async (req, res) => {
 
     const APIKey = req.header('X-API-Key')
 
-    if (APIKey != process.env.API_KEY){
-        const response = { error: "Must provide an API key"}
+    if (APIKey != process.env.API_KEY) {
+        const response = { error: "Must provide an API key" }
         return res.status(403).json(response)
-        
-    }    
-    
-    const [ dbResult ] = await db('score').insert({        
+
+    }
+
+    const [dbResult] = await db('score').insert({
         ...req.body
     }).returning('*')
 
@@ -21,12 +21,12 @@ const createScore = async (req, res) => {
 }
 
 const getScores = async (req, res) => {
-    const queryResult = await db.select('*').from('score')    
+    const queryResult = await db.select('*').from('score')
     return res.status(200).json(queryResult)
 }
 
-const deleteScores =  async (req, res) => {
+const deleteScores = async (req, res) => {
     db('accounts').del()
-    return res.status(200).json({message: "Scoreboard erased"})
+    return res.status(200).json({ message: "Scoreboard erased" })
 }
-module.exports = { createScore, getScores }
+module.exports = { createScore, getScores, deleteScores }
