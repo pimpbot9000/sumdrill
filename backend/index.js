7188aa6a3d9a96a194cbcede7fc15c2aadf17962
pimpbot9000 <t.valimaki@gmail.com>
2021-12-03 16:18:03 +0200

    untract .env

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 const router = require('./src/routes/routes')
 const db = require('./src/db/db')
+const e = require('express')
 
 // run migrations when the app starts
 db.migrate.latest()
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 8080
 
 if (process.env.NODE_ENV == "development" || process.env.NODE_ENV == "docker_compose") {
     app.use(cors())
+} else {
+    console.log("CORS disabled on production")
 }
 
 app.use(express.static('../build'))
